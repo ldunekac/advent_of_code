8b53aaea1cfe5553550a5507d6800e5a50164434
Luke Dunekacke <ldunekacke@gmail.com>
2023-08-07 20:33:09 -0600

    2015 day 4 complete

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ mod year_2015;
 fn main() {
     // year_2015::day1::day1::solve();
     // year_2015::day2::day2::solve();
-    year_2015::day3::day3::solve()
+    // year_2015::day3::day3::solve();
+    year_2015::day4::day4::solve();
 }
---
+++ src/year_2015/day4/day4.rs
@@ -0,0 +1,28 @@
+fn hash_that_starts_with(key: &str, starts: &str) -> usize {
+    let max_num: usize = 200000000;
+    for i in 0..max_num {
+        let ans = format!("{}{}", key, i);
+        let md5_hash = md5::compute(ans);
+        if format!("{:x}", md5_hash).starts_with(starts) {
+            return i as usize
+        }
+    }
+    return 0;
+}
+
+fn part1(key: &str) -> usize {
+    return hash_that_starts_with(key, "00000");
+}
+
+fn part2(key: &str) -> usize {
+    return hash_that_starts_with(key, "000000");
+}
+
+
+pub fn solve() {
+    println!("Number to make md5 hash start with 5 zeros is {}", part1(&"abcdef"));
+    println!("Number to make md5 hash start with 5 zeros is {}", part1(&"pqrstuv"));
+
+    println!("Number to make md5 hash start with 5 zeros is {}", part1(&"bgvyzdsv"));
+    println!("Number to make md5 hash start with 6 zeros is {}", part2(&"bgvyzdsv"));
+}
