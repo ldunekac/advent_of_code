cbc53ff4e02008ed14e8fbbf20470081124fc45e
Luke Dunekacke <ldunekacke@gmail.com>
2023-08-07 18:21:20 -0600

    day 1 for year 2015 initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate core;
+
+mod year_2015;
+
+fn main() {
+    year_2015::day1::day1::day1_2015();
+}
---
+++ src/year_2015/day1/day1.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn to_num(c: &u8) -> i32 {
+    return match *c as char {
+        ')' => -1,
+        '(' => 1,
+        _ => panic!("WAT")
+    }
+}
+
+fn part1(input: &Vec<u8>) {
+    let floor = input.into_iter().map(|x| to_num(x)).reduce(|a, b| a + b).unwrap();
+    println!("You are on floor {}", floor);
+}
+
+fn part2(input: &Vec<u8>) {
+    let mut current_floor = 0;
+
+    for (idx, op) in input.iter().enumerate() {
+        current_floor += to_num(&op);
+        if current_floor < 0 {
+            println!("Entered Basement at position {}", idx + 1);
+            break;
+        }
+    }
+}
+
+pub fn day1_2015() {
+    let example_input = fs::read_to_string("src/year_2015/day1/example.txt").unwrap().chars().map(|x| x as u8).collect::<Vec<u8>>();
+    part1(&example_input);
+
+    let input = fs::read_to_string("src/year_2015/day1/input.txt").unwrap().chars().map(|x| x as u8).collect::<Vec<u8>>();
+    part1(&input);
+
+    part2(&example_input);
+    part2(&input);
+}
