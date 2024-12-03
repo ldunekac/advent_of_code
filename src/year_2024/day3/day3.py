8a10b972e5c421205b1a3c62f5c7e23109852ecc
Luke Dunekacke <luke.dunekacke@einstein-tech.com>
2024-12-03 07:08:04 -0700

    2024 day 3

--- src/year_2024/day3/day3.py
+++ src/year_2024/day3/day3.py
@@ -1,8 +1,81 @@
 import sys
+import re
+
+def do_ranges(memory):
+    dos = [0] + [m.start() for m in re.finditer("do\(\)", memory)]
+    donts = [m.start() for m in re.finditer("don\'t\(\)", memory)]
+
+    good_ranges = []
+    donts_max = max(donts)
+    for do in dos:
+        if len(good_ranges) > 0:
+            last_range = good_ranges[-1]
+            if last_range[0] <= do <= last_range[1]:
+                # The current do is between a do and don't skip the due
+                continue
+        range_start = do
+        if do > donts_max:
+            good_ranges.append((range_start, len(memory)))
+            break
+        else:
+            range_stop = min([d for d in donts if do < d])
+            good_ranges.append((range_start, range_stop))
+
+    # print(dos)
+    # print(donts)
+    # print(good_ranges)
+
+    return good_ranges
 
 
-def solution2(levels):
-    return 0
+def in_good_range(index, good_ranges):
+    for start, end in good_ranges:
+        if start <= index <= end:
+            return True
+    return False
+
+def solution2(memory):
+    total = 0
+    good_ranges = do_ranges(memory)
+
+    multis = [0] + [m.start() for m in re.finditer("mul\(", memory)]
+
+    for multi in multis:
+        index = multi + 3
+        if not in_good_range(index, good_ranges):
+            continue
+
+        num1=""
+        i = 0
+        error = True
+        for i in range(4):
+            next_char = memory[index+i+1]
+            if next_char >= "0" and next_char <= "9":
+                num1 += next_char
+            elif next_char == ",":
+                error = False
+                break
+            else:
+                break
+        if error:
+            continue
+
+        index = index + i + 1
+        num2= ""
+        error = True
+        for i in range(4):
+            next_char = memory[index+i+1]
+            if next_char >= "0" and next_char <= "9":
+                num2 += next_char
+            elif next_char == ")":
+                error = False
+                break
+            else:
+                break
+        if error:
+            continue
+        total += int(num1) * int(num2)
+    return total
 
 def solution1(memory):
     total = 0
