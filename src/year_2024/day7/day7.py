fb56587c6de6928876d38e5702d5bc2ffea066b4
Luke <ldunekacke@gmail.com>
2024-12-13 07:59:27 -0700

    template update

--- src/year_2024/day7/day7.py
+++ src/year_2024/day7/day7.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from alive_progress import alive_bar
 
 
@@ -89,8 +90,10 @@ def main():
     sol_num = int(sys.argv[1])
     input_file = sys.argv[2]
     input_data = parse_input(input_file)
+    start = time.perf_counter()
     answer = solution1(input_data) if sol_num == 1 else solution2(input_data)
-
+    stop = time.perf_counter()
+    print(f"Took: {stop-start} seconds")
     print(f"Solution to Part {sol_num} for {input_file} is: {answer}")
 
 if __name__ == "__main__":
--- src/year_2024/template.py
+++ src/year_2024/template.py
@@ -1,23 +1,45 @@
 import sys
+import time
+from alive_progress import alive_bar
 
+<<<<<<< Updated upstream
 
 def solution2(grid):
     return ""
 
 
 def solution1(grid):
+=======
+def solution2(input):
+    with alive_bar(len(input)) as bar:
+        bar()
+    return ""
+
+def solution1(input):
+    with alive_bar(len(input)) as bar:
+        bar()
+>>>>>>> Stashed changes
     return ""
 
 
 def parse_input(file_name):
-    return ""
+    with open(file_name, "r") as file:
+        for line in file:
+            continue
 
 
 def main():
     sol_num = int(sys.argv[1])
     input_file = sys.argv[2]
     input_data = parse_input(input_file)
+    start = time.perf_counter()
     answer = solution1(input_data) if sol_num == 1 else solution2(input_data)
+<<<<<<< Updated upstream
+=======
+    stop = time.perf_counter()
+    print(f"Took: {stop-start} seconds")
+
+>>>>>>> Stashed changes
     print(f"Solution to Part {sol_num} for {input_file} is: {answer}")
 
 if __name__ == "__main__":
